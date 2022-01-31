9d6cd8a7679a0b299bc69378c6328ef5c641d0dc
AdilahSyaza <adilah.syaza55@gmail.com>
2022-02-01 00:12:08 +0800

    fixed error viewForum in views.py of sharing module + format docs

--- sharing/views.py
+++ sharing/views.py
@@ -159,11 +159,9 @@ def viewForum(request, pk):
         searchshare=Feed.objects.raw('select * from Feed where Skill="'+Skill+'" and State="'+State+'"')
         return render(request, 'Forum.html', {'feed': feed, 'data':data,'feed':searchshare})
     try:
-            data=Feed.objects.all()
             data = Group_tbl.objects.get(id=pk)
-            person=Person.objects.get(Email=request.session['Email'])
-            searchshare=Feed.objects.raw('select * from Feed') 
-            return render(request,'Forum.html', {'person':person,'data':data})
+            feed = Feed.objects.filter(Group = data)
+            return render(request,'Forum.html', {'feed':feed,'data':data})
     except Feed.DoesNotExist:
             raise Http404('Data does not exist') 
 
